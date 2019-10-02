493c694c119b9318ef52e2dd9fd65d1e09390381
Rob Van Aert <rob_van_aert@yahoo.com>
2019-10-02 08:53:10 +0200

    [homeComponent]: remove apicode for firestore

--- Verzet/src/environments/environment.prod.ts
+++ Verzet/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCSv8Ub2duKGn00l6mmTJyRrzrNSUybXwY",
-    authDomain: "het-verzet.firebaseapp.com",
-    databaseURL: "https://het-verzet.firebaseio.com",
-    projectId: "het-verzet",
-    storageBucket: "het-verzet.appspot.com",
-    messagingSenderId: "872848382372",
-    appId: "1:872848382372:web:6fae51fcbaeb6749a1c931"
-  }
 };
