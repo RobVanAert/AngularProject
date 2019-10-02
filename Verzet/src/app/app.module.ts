d0622c052f6911a27bdd11d1009be11b3f661cdd
Rob Van Aert <rob_van_aert@yahoo.com>
2019-10-02 13:51:14 +0200

    [footer]: add basic footer

--- Verzet/src/app/app.module.ts
+++ Verzet/src/app/app.module.ts
@@ -3,21 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
-import { AppRoutingModule } from '../modules/app-routing.module';
-import { AppComponent } from '../components/app/app.component';
-import { HeaderComponent } from '../components/header/header.component';
-import { NavigationComponent } from '../components/header/navigation/navigation.component';
-import { MaterialModule } from '../modules/shared/material.module';
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/header/navigation/navigation.component';
+import { MaterialModule } from './modules/shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { MatListModule } from '@angular/material/list';
     HeaderComponent,
     NavigationComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
