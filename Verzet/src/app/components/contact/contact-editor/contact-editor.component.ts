738f99aeb83857242df1806d9824c0446c94c185
Rob Van Aert <rob_van_aert@yahoo.com>
2019-10-09 21:21:56 +0200

    [contact]: add membershipNumber VWB depending (enable/disable) on member of VWB

--- Verzet/src/app/components/contact/contact-editor/contact-editor.component.ts
+++ Verzet/src/app/components/contact/contact-editor/contact-editor.component.ts
@@ -1,50 +1,59 @@
-import { Component} from '@angular/core';
-import { FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, Validators, FormGroupDirective, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-editor',
   templateUrl: './contact-editor.component.html',
   styleUrls: ['./contact-editor.component.sass']
 })
-export class ContactEditorComponent {
-  contactGroup = this.formBuilder.group ({ 
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required,Validators.email]],
-    subject: ['', [Validators.required, Validators.minLength(5)]],
-    message: ['', Validators.required],
-  })
+export class ContactEditorComponent implements OnInit {
   
-    constructor(private formBuilder: FormBuilder) { }
+  contactGroup: FormGroup;
   
-    get name() {return this.contactGroup.get('name');}
-    get email() {return this.contactGroup.get('email');}
-    get subject() {return this.contactGroup.get('subject');}
-    get message() {return this.contactGroup.get('message');}
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm()
+  }
+
+  createForm() {
+    this.contactGroup = this.formBuilder.group ({ 
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required,Validators.email]],
+      subject: ['', [Validators.required, Validators.minLength(5)]],
+      message: ['', Validators.required],
+    })
+  }
+
+  get name() {return this.contactGroup.get('name');}
+  get email() {return this.contactGroup.get('email');}
+  get subject() {return this.contactGroup.get('subject');}
+  get message() {return this.contactGroup.get('message');}
   
-    getNameErrorMessage() {
-      return this.name.hasError('required') ? 'u moet uw naam opgeven' :
-            this.name.hasError('minlength') ? 'uw naam moet minstens 3 karakters lang zijn' :''
-          }
+  getNameErrorMessage() {
+    return this.name.hasError('required') ? 'u moet uw naam opgeven' :
+          this.name.hasError('minlength') ? 'uw naam moet minstens 3 karakters lang zijn' :''
+  }
   
-    getMailErrorMessage() {
-      return this.email.hasError('required') ? 'u moet een e-mail opgeven' :
-            this.email.hasError('email') ? 'dit is geen geldig e-mail adres' :''
-    }
+  getMailErrorMessage() {
+    return this.email.hasError('required') ? 'u moet een e-mail opgeven' :
+           this.email.hasError('email') ? 'dit is geen geldig e-mail adres' :''
+  }
   
-    getSubjectErrorMessage() {
-      return this.subject.hasError('required') ? 'u moet een onderwerp opgeven' :
-            this.subject.hasError('minlength') ? 'het onderwerp moet minstens 5 karakters lang zijn' :''
-    }
+  getSubjectErrorMessage() {
+    return this.subject.hasError('required') ? 'u moet een onderwerp opgeven' :
+          this.subject.hasError('minlength') ? 'het onderwerp moet minstens 5 karakters lang zijn' :''
+  }
   
-    getMessageErrorMessage() {
-      return this.message.hasError('required') ? 'u moet een bericht opgeven' : ''
-    }
+  getMessageErrorMessage() {
+    return this.message.hasError('required') ? 'u moet een bericht opgeven' : ''
+  }
     
   
-    onSubmit(formDirective: FormGroupDirective): void {
-      console.log(this.name.value, this.email.value, this.subject.value, this.message.value);
-      formDirective.resetForm();
-      this.contactGroup.reset();
+  onSubmit(formDirective: FormGroupDirective) {
+    console.log(this.name.value, this.email.value, this.subject.value, this.message.value);
+    formDirective.resetForm();
+    this.contactGroup.reset();
     }
 
 }
--- Verzet/src/app/components/contact/new-member-editor/new-member-editor.component.ts
+++ Verzet/src/app/components/contact/new-member-editor/new-member-editor.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import { FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, Validators, FormGroupDirective, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-member-editor',
@@ -7,23 +7,34 @@ import { FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
   styleUrls: ['./new-member-editor.component.sass']
 })
 
-export class NewMemberEditorComponent {
+export class NewMemberEditorComponent implements OnInit{
 
-  newMemberGroup = this.formBuilder.group({
-    salutation: [''],
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.required,Validators.email]],
-    memberVWB: [''],
-  })
+  newMemberGroup: FormGroup;
   
   constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(){
+    this.createForm();
+    this.onChangeMembership();
+  }
+  
+  createForm(){
+    this.newMemberGroup = this.formBuilder.group({
+      salutation: [''],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      memberVWB: [false],
+      membershipNumberVWB: [{value:'', disabled: true}]
+    })
+  }
 
   get salutation() {return this.newMemberGroup.get('salutation');}
   get firstName() {return this.newMemberGroup.get('firstName');}
   get lastName() {return this.newMemberGroup.get('lastName');}
   get email() {return this.newMemberGroup.get('email');}
   get memberVWB() {return this.newMemberGroup.get('memberVWB');}
+  get membershipNumberVWB() {return this.newMemberGroup.get('membershipNumberVWB');}
 
   getFirstNameErrorMessage() {
     return this.firstName.hasError('required') ? 'u moet uw voornaam opgeven' :'';         
@@ -36,6 +47,16 @@ export class NewMemberEditorComponent {
   getMailErrorMessage() {
     return this.email.hasError('required') ? 'u moet een e-mail opgeven' :
           this.email.hasError('email') ? 'dit is geen geldig e-mail adres' :'';
+  }
+
+  onChangeMembership() {
+    this.memberVWB.valueChanges.subscribe(member =>{
+      if (member) {
+        this.membershipNumberVWB.enable();
+      } else{
+        this.membershipNumberVWB.disable();
+      }
+    })
   }
 
   onSubmit(formDirective: FormGroupDirective): void {
